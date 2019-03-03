6f0ffae492d0ab23377f5fdfce842d0af991299c
AtlasZ98 <songziyu98@gmail.com>
2019-03-02 22:18:54 -0500

    added distance in infowindows

--- liftknockoff/ajax.js
+++ liftknockoff/ajax.js
@@ -1,5 +1,9 @@
 var myLat = 0;
 var myLng = 0;
+var myLatlng;
+var nearest_mile = 0;
+var METER_TO_MILES = 1609.34;
+
 var myPosition = new google.maps.LatLng(myLat, myLng);
 
 var myOptions = {
@@ -8,10 +12,11 @@ var myOptions = {
     mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 var map;
-var marker;
-var infowindow = new google.maps.InfoWindow();
 
 var request = new XMLHttpRequest();
+
+var weinermobile_status = "The Weinermobile is nowhere to be seen.";
+var nearest_vehicle = "There is no other vehicle near me.";
 
 function init() {
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
@@ -23,6 +28,7 @@ function getLocations() {
         navigator.geolocation.getCurrentPosition(function(position) {
             myLat = position.coords.latitude;
             myLng = position.coords.longitude;
+            myLatlng = new google.maps.LatLng(myLat, myLng);
             renderMap();
             getOtherLocations();
         });
@@ -37,34 +43,74 @@ function renderMap() {
     myPosition = new google.maps.LatLng(myLat, myLng);
     map.panTo(myPosition);
 
-    marker = new google.maps.Marker({
+    var myMarker = new google.maps.Marker({
         icon: "icons/me.png",
         position: myPosition,
-        title: "The Weinermobile is X miles away from me!"
     });
-    marker.setMap(map);
+    myMarker.setMap(map);
         
-    // Open info window on click of marker
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(marker.title);
-        infowindow.open(map, marker);
+    var infowindow = new google.maps.InfoWindow();
+    myMarker.addListener('click', function() {
+        infowindow.setContent(weinermobile_status + "<br />" + nearest_vehicle);
+        infowindow.open(map, myMarker);
     });
 }
 
 function getOtherLocations() {
     request.open("POST", "https://hans-moleman.herokuapp.com/rides", true);
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200) {
-            console.log("Got the data back!");
             data = request.responseText;
-            console.log(data);
+            var arr = JSON.parse(data);
+            var markers = new Array([arr.vehicles.length]);
+            var vLatlngs = new Array([arr.vehicles.length]);
+            var infowindows = new Array([arr.vehicles.length]);
+
+            console.log(arr);
+            for (var i in arr.vehicles) {
+                vLatlngs[i] = new google.maps.LatLng(arr.vehicles[i].lat, arr.vehicles[i].lng);
+                var distance = google.maps.geometry.spherical.computeDistanceBetween(myLatlng, vLatlngs[i]) / METER_TO_MILES;
+
+                if (arr.vehicles[i].username == "WEINERMOBILE") {
+                    weinermobile_status = "The Weinermobile is " + distance + " miles from me !";
+                    markers[i] = new google.maps.Marker({
+                        icon: "icons/weinermobile.png",
+                        position: vLatlngs[i],
+                    });
+                    markers[i].setMap(map);
+
+                } else {
+                    markers[i] = new google.maps.Marker({
+                        icon: "icons/passenger.png",
+                        position: vLatlngs[i],
+                    });
+                    markers[i].setMap(map);
+                    if (distance > nearest_mile) {
+                        nearest_mile = distance;
+                        nearest_vehicle = "The neareset vehicles is " + nearest_mile + " away.";
+                    }
+  
+                }
+                markers[i].addListener('click', function() {
+                    infowindows[i] = new google.maps.InfoWindow();
+                    infowindows[i].setContent("This vehicle is " + arr.vehicles[i].username + ".<br /> It is " + distance + " miles from me.");
+                    infowindows[i].open(map, markers[i]);
+                });
+            }
+
         }
+
+
         else if (request.readyState == 4 && request.status != 200) {
-        }
-        else {
-            console.log("In progress...");
+            alert("Server responded with a bad status code!")
         }
     };
-    console.log("username=pCH9E2zt&lat=" + myLat + "&lng=" + myLng);
     request.send("username=pCH9E2zt&lat=" + myLat + "&lng=" + myLng);
 }
+
+
+
+
+
+
